ffe9aa0efbcb1217c8959fae4582175733a6fcb6
User <gabrielkleber006@gmail.com>
2021-07-08 22:45:08 -0300

    desafio 6 teste

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-const colorPalette = document.querySelector("#color-pallete")
-const boxColor = document.querySelector(".color")
 
-boxColor[0].style.backgroundColor = "red"
+//criando linhas
+const pxBoardList = document.querySelector("#pixel-board")
+function addLi(){
+
+    for(let i = 1; i <= 5; i += 1){
+        const pxColuna= document.createElement("ul")
+        pxColuna.className ="coluna"
+        pxBoardList.appendChild(pxColuna)
+        
+    for(let i2 = 1; i2 <= 5; i2 += 1){
+        const pxLinha = document.createElement("li")
+        pxLinha.className ="pixel"
+        pxColuna.appendChild(pxLinha)
+    
+    }
+
+    }
+}
+addLi()
+
+window.onload = function selectBlack(){
+    let boxBlack = document.querySelector(".boxColor1")
+    boxBlack.classList.add('selected')
+    }
